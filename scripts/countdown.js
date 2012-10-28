d73738bf9a3b04023512e1a9a819648b5480785c
Septimus <septimus98@gmail.com>
2012-10-28 14:18:17 -0400

    Update in countdown timer

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -11,12 +11,15 @@ function updateTime() {
 	mins  = Math.floor( diff / (1000*60) );
 	secs  = Math.floor( diff / 1000 );
 
-	dd = days;
-	hh = hours - days  * 24;
-	mm = mins  - hours * 60;
-	ss = secs  - mins  * 60;
+	d = days;
+	h = hours - days  * 24;
+	m = mins  - hours * 60;
+	s = secs  - mins  * 60;
 
 	// document.getElementById("worldcup_countdown_time").innerHTML = dd + ' days<br/>' + hh + ' hours<br/>' + mm + ' minutes<br/>' + ss + ' seconds' ;
-	document.getElementById("countdown").innerHTML = dd + ' days ' + hh + ' hours ' + mm + ' minutes ' + ss + ' seconds ';
+	document.getElementById("days").innerHTML = d;
+	document.getElementById("hours").innerHTML = h;
+	document.getElementById("min").innerHTML = m;
+	document.getElementById("sec").innerHTML = s;
 }
 setInterval('updateTime()', 1000 );
